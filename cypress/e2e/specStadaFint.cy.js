73ba73f70387061e9ce3115e089c5b4ce9877ffd
Daniella Alolo <daniella.alolo@hotmail.com>
2024-05-02 22:12:15 +0200

    fixed error and added test

--- cypress/e2e/specStadaFint.cy.js
+++ cypress/e2e/specStadaFint.cy.js
@@ -1,7 +1,34 @@
-describe('testing navbar links', () => {
-  it('passes', () => {
-    cy.visit('../../index.html')
 
-    cy.get('.nav-area > ul > li').click({ multiple: true })
-  })
-})
+describe("Testing CleaningCompany page", () => {
+  it("Opens the correct page", () => {
+    cy.visit("../../index.html");
+
+    cy.get(".nav-area >ul >li").contains("Login").click();
+    cy.url().should("include", "/login.html");
+  });
+});
+
+it("Contains correct Header text", () => {
+  cy.visit("../../index.html");
+
+  cy.contains("Cleaning Company").should("exist");
+});
+
+it("Contains visable nav-links", () => {
+  cy.visit("../../index.html");
+
+  cy.get(".nav-area >ul >li").each(($li) => {
+    cy.wrap($li).should("be.visible");
+  });
+});
+
+it("Displays hamburgermeny in smaller screens ", () => {
+  cy.visit("../../index.html");
+  cy.viewport("iphone-x");
+
+  cy.get(".btn-area").should("be.visible");
+  cy.get(".btn-area").click();
+  cy.get(".nav-area ul").should("be.visible");
+  cy.get(".btn-area").click();
+  cy.get(".nav-area ul").should("not.be.visible");
+});

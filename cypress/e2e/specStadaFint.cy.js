f15bb46f00ab2d10eb089eb120fa82b9fd297811
Daniella Alolo <daniella.alolo@hotmail.com>
2024-05-06 21:23:17 +0200

    added beforeEach

--- cypress/e2e/specStadaFint.cy.js
+++ cypress/e2e/specStadaFint.cy.js
@@ -1,29 +1,26 @@
-
 describe("Testing CleaningCompany page", () => {
-  it("Opens the correct page", () => {
-    cy.visit("../../index.html");
 
+  beforeEach(() => {
+    cy.visit("../../index.html");
+  });
+
+  it("Opens the correct page", () => {
     cy.get(".nav-area >ul >li").contains("Login").click();
     cy.url().should("include", "/login.html");
   });
-});
+
 
 it("Contains correct Header text", () => {
-  cy.visit("../../index.html");
-
-  cy.contains("Cleaning Company").should("exist");
+  cy.get("h1").contains("Cleaning Company").should("exist");
 });
 
 it("Contains visable nav-links", () => {
-  cy.visit("../../index.html");
-
   cy.get(".nav-area >ul >li").each(($li) => {
     cy.wrap($li).should("be.visible");
   });
 });
 
 it("Displays hamburgermeny in smaller screens ", () => {
-  cy.visit("../../index.html");
   cy.viewport("iphone-x");
 
   cy.get(".btn-area").should("be.visible");
@@ -31,4 +28,5 @@ it("Displays hamburgermeny in smaller screens ", () => {
   cy.get(".nav-area ul").should("be.visible");
   cy.get(".btn-area").click();
   cy.get(".nav-area ul").should("not.be.visible");
+});
 });
